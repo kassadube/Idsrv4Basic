eba72bde3bc2db5844aef9f0c1e22bafa736dc29
kassadube <noamsrb@yahoo.com>
2019-12-24 11:59:31 +0200

    add client project with login layout

---
+++ src/Toll.portal/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Toll.portal.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+       
+    }
+}
