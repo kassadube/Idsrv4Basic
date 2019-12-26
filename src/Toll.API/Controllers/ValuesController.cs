74a4f8a1305255537f02ccbd152f6b404d24014f
kassadube <noamsrb@yahoo.com>
2019-12-26 11:34:06 +0200

    add Toll.api

---
+++ src/Toll.API/Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Toll.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/Values
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var claims = User.Claims.Select(x => $"{x.Type}: {x.Value}");
+            return Ok(new
+            {
+                Name = "Values API",
+                Claims = claims.ToArray()
+
+            });
+        }
+
+       
+    }
+}
